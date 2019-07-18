cb5f3d34c5028d9be0992281d6661ec5ac7f45e4
Cigdem Coskuner <coskuner.cig@gmail.com>
2019-07-17 19:01:45 -0700

    Updates

---
+++ precache-manifest.455bfe30d06878b8f3ff5e5ae978acbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db7e3852740e5d675c00f6ff8587dac",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "ac393f75b4da80457726",
+    "url": "/smart-brain/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "bafa463a97675ee71a8d",
+    "url": "/smart-brain/static/css/main.5d2fc01e.chunk.css"
+  },
+  {
+    "revision": "ac393f75b4da80457726",
+    "url": "/smart-brain/static/js/2.33497142.chunk.js"
+  },
+  {
+    "revision": "bafa463a97675ee71a8d",
+    "url": "/smart-brain/static/js/main.95499ea4.chunk.js"
+  },
+  {
+    "revision": "b9b46f64ddcbca33fcc3",
+    "url": "/smart-brain/static/js/runtime~main.e63bf16e.js"
+  }
+]);
